639070aa121f7867b3f59d48660195e50b541b98
MER-DEV <82384469+MER-DEV@users.noreply.github.com>
2021-04-16 22:56:41 +0300

    Add files via upload

--- files_MER/ReplyBot.lua
+++ files_MER/ReplyBot.lua
@@ -1,3 +1,4 @@
+DevBotTele:
 local function Reply(msg)
 local text = msg.content_.text_
 if not database:get(bot_id..'Reply:Mute'..msg.chat_id_) then
@@ -6,15 +7,15 @@ if DevBot(msg) then
 if text == 'هلو' then
 send(msg.chat_id_, msg.id_,'هلا مطوري')
 return false
-end --- if line 6
+end 
 if text == 'مرحبا' then
 send(msg.chat_id_, msg.id_,'ههلا كلبي')
 return false
-end --- if line 10
+end 
 if text == 'باي' then
 send(msg.chat_id_, msg.id_,'الله وياك حياتي')
 return false
-end --- if line 15
+end
 end --- if line 4 
 -- ردود غير المطور -- 
 if not DevBot(msg) then
@@ -29,16 +30,18 @@ name = math.random(#namebot)
 send(msg.chat_id_, msg.id_, namebot[name]) 
 return false 
 end
+end
+
 if text == 'تفعيل ردود البوت' and Owner(msg) then
 database:del(bot_id..'Reply:Mute'..msg.chat_id_)
 send(msg.chat_id_, msg.id_,'*⋄︙تم تفعيل ردود البوت*')
 return false
-end --- if line 35
+end
 if text == 'تعطيل ردود البوت' and Owner(msg) then
 database:set(bot_id..'Reply:Mute'..msg.chat_id_,true)
 send(msg.chat_id_, msg.id_,'*⋄︙تم تعطيل ردود البوت*')
 return false
-end --- if line 40
+end 
 end --- if line 1
 return {
 B = Reply
