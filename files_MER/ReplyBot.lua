fd93c39021412276f8f9cefc7883579421e0dd3d
MER-DEV <82384469+MER-DEV@users.noreply.github.com>
2021-04-16 23:16:27 +0300

    Update ReplyBot.lua

--- files_MER/ReplyBot.lua
+++ files_MER/ReplyBot.lua
@@ -2,28 +2,25 @@ local function Reply(msg)
 local text = msg.content_.text_
 if not database:get(bot_id..'Reply:Mute'..msg.chat_id_) then
 if DevBot(msg) then
--- ردود المطور -- 
-if text == 'هلو' then
-send(msg.chat_id_, msg.id_,'هلا مطوري')
-return false
-end 
-if text == 'مرحبا' then
-send(msg.chat_id_, msg.id_,'ههلا كلبي')
-return false
-end 
-if text == 'باي' then
-send(msg.chat_id_, msg.id_,'الله وياك حياتي')
-return false
+if text == "سلام عليكم"  then     
+local texting = {'6💘','عليـگم h💞','وعليڪم g🍇'}
+send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
+return false 
 end
-end --- if line 4 
+
+end
 -- ردود غير المطور -- 
 if not DevBot(msg) then
+
 if text == "سلام عليكم"  then     
 local texting = {'ياهلا وعليڪم السلام عمغي 💘','عليـگم آلسـلآم تآج رآسـي 💞','وعليڪم سلام ياڪمر 💕','وعـليـكم الـسلام يـحلو 🍇'}
 send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
 return false 
 end
+
 end
+
+
 if text == 'تفعيل ردود البوت' and Owner(msg) then
 database:del(bot_id..'Reply:Mute'..msg.chat_id_)
 send(msg.chat_id_, msg.id_,'*⋄︙تم تفعيل ردود البوت*')
@@ -33,8 +30,8 @@ if text == 'تعطيل ردود البوت' and Owner(msg) then
 database:set(bot_id..'Reply:Mute'..msg.chat_id_,true)
 send(msg.chat_id_, msg.id_,'*⋄︙تم تعطيل ردود البوت*')
 return false
-end 
+end
+
+------------
 end --- if line 1
-return {
-B = Reply
-}
+return {B = Reply}
